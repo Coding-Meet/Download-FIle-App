2cd4e69db6d57459b6c856df00d9991298ec39a5
coding meet <codingmeet26@gmail.com>
2023-06-25 14:54:28 +0530

    Initial commit

---
+++ app/src/main/java/com/coding/meet/downloadfileapp/MainActivity.kt
@@ -0,0 +1,176 @@
+package com.coding.meet.downloadfileapp
+
+import android.app.DownloadManager
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+    private val multiplePermissionId = 14
+    private val multiplePermissionNameList = if (Build.VERSION.SDK_INT >= 33) {
+        arrayListOf()
+    } else {
+        arrayListOf(
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        )
+    }
+    private lateinit var edUrl: EditText
+    private lateinit var snackbar: Snackbar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        edUrl = findViewById(R.id.edUrl)
+        val rootView = findViewById<View>(R.id.rootView)
+        snackbar = Snackbar.make(
+            rootView,
+            "No Internet Connection",
+            Snackbar.LENGTH_INDEFINITE
+        ).setAction("Setting") {
+            startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+        }
+        val downloadFileBtn = findViewById<Button>(R.id.downloadFileBtn)
+        downloadFileBtn.setOnClickListener {
+            if (checkMultiplePermission()) {
+                doOperation()
+            }
+        }
+
+    }
+
+    private fun doOperation() {
+        Toast.makeText(
+            this,
+            "All Permission Granted Successfully!",
+            Toast.LENGTH_SHORT
+        ).show()
+        if (edUrl.text.toString().trim().isNotEmpty()) {
+            if (isConnected(this)) {
+                if (snackbar.isShown) {
+                    snackbar.dismiss()
+                }
+                download(edUrl.text.toString().trim())
+            } else {
+                snackbar.show()
+            }
+        } else {
+            edUrl.error = "Required"
+        }
+    }
+
+    private fun download(url: String) {
+        val folder = File(
+            Environment.getExternalStorageDirectory().toString() + "/Download/Image"
+        )
+        if (!folder.exists()) {
+            folder.mkdirs()
+        }
+        Toast.makeText(this, "Download Started", Toast.LENGTH_SHORT).show()
+        val fileName = url.split("/").last()
+
+        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        val request = DownloadManager.Request(Uri.parse(url))
+        request.setAllowedNetworkTypes(
+            DownloadManager.Request.NETWORK_WIFI or
+                    DownloadManager.Request.NETWORK_MOBILE
+        )
+        request.setTitle(fileName)
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        request.setDestinationInExternalPublicDir(
+            Environment.DIRECTORY_DOWNLOADS,
+            "Image/$fileName"
+        )
+        downloadManager.enqueue(request)
+
+    }
+
+
+    private fun checkMultiplePermission(): Boolean {
+        val listPermissionNeeded = arrayListOf<String>()
+        for (permission in multiplePermissionNameList) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                listPermissionNeeded.add(permission)
+            }
+        }
+        if (listPermissionNeeded.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                listPermissionNeeded.toTypedArray(),
+                multiplePermissionId
+            )
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == multiplePermissionId) {
+            if (grantResults.isNotEmpty()) {
+                var isGrant = true
+                for (element in grantResults) {
+                    if (element == PackageManager.PERMISSION_DENIED) {
+                        isGrant = false
+                    }
+                }
+                if (isGrant) {
+                    // here all permission granted successfully
+                    doOperation()
+                } else {
+                    var someDenied = false
+                    for (permission in permissions) {
+                        if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                                this,
+                                permission
+                            )
+                        ) {
+                            if (ActivityCompat.checkSelfPermission(
+                                    this,
+                                    permission
+                                ) == PackageManager.PERMISSION_DENIED
+                            ) {
+                                someDenied = true
+                            }
+                        }
+                    }
+                    if (someDenied) {
+                        // here app Setting open because all permission is not granted
+                        // and permanent denied
+                        appSettingOpen(this)
+                    } else {
+                        // here warning permission show
+                        warningPermissionDialog(this) { _: DialogInterface, which: Int ->
+                            when (which) {
+                                DialogInterface.BUTTON_POSITIVE ->
+                                    checkMultiplePermission()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/coding/meet/downloadfileapp/utils.kt
@@ -0,0 +1,58 @@
+package com.coding.meet.downloadfileapp
+
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import android.widget.Toast
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+fun appSettingOpen(context: Context){
+    Toast.makeText(
+        context,
+        "Go to Setting and Enable All Permission",
+        Toast.LENGTH_LONG
+    ).show()
+
+    val settingIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+    settingIntent.data = Uri.parse("package:${context.packageName}")
+    context.startActivity(settingIntent)
+}
+
+fun warningPermissionDialog(context: Context,listener : DialogInterface.OnClickListener){
+    MaterialAlertDialogBuilder(context)
+        .setMessage("All Permission are Required for this app")
+        .setCancelable(false)
+        .setPositiveButton("Ok",listener)
+        .create()
+        .show()
+}
+
+fun isConnected(context: Context):Boolean{
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    return when{
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> {
+            val activeNetwork = connectivityManager.activeNetwork ?: return false
+            val cap = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+            when {
+                cap.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                cap.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        }
+        else -> {
+            // use Deprecated method only on older devices
+            val activeNetwork = connectivityManager.activeNetworkInfo ?: return false
+            return when (activeNetwork.type){
+                ConnectivityManager.TYPE_MOBILE -> true
+                ConnectivityManager.TYPE_WIFI -> true
+                ConnectivityManager.TYPE_VPN -> true
+                else -> false
+            }
+        }
+    }
+}
